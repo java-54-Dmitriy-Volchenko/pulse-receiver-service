a2cec24777f795a58d6d2b8ebb6703476a25f527
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-10-16 19:47:27 +0300

    adds dependency and environment variables to work with dynamoDB

--- src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
+++ src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
@@ -1,11 +1,20 @@
 package telran.monitoring.pulse;
 import java.net.*;
 import java.util.Arrays;
+
 public class PulseReceiverAppl {
 private static final int PORT = 5000;
 private static final int MAX_BUFFER_SIZE = 1500;
 static DatagramSocket socket;
+//System.getenv("LOGGING_LEVEL")
+		//System.getenv("MAX_THRESHOLD_PULSE_VALUE")
+		//System.getenv("MIN_THRESHOLD_PULSE_VALUE")
+		//System.getenv("WARN_MAX_PULSE_VALUE")
+		//System.getenv("WARN_MIN_PULSE_VALUE")
+	
 	public static void main(String[] args) throws Exception{
+		
+		
 		socket  = new DatagramSocket(PORT);
 		byte [] buffer = new byte[MAX_BUFFER_SIZE];
 		while(true) {
